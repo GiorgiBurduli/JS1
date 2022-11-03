9789d37145091d84c0ae2d8a99bc52a4599f51d6
Giorgi Burduli <giorgiburduli013@gmail.com>
2022-11-03 17:50:32 +0400

    js 1

---
+++ main.js
@@ -0,0 +1,17 @@
+const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+
+ shopping.push('tea', 'coffee', 'chocolate');  //itemis damateba arrayshi//
+
+ shopping.unshift('banana');      //itemis pirvel adgilze damateba//
+ shopping.pop();                  //bolo itemis amogeba// 
+
+ 
+
+ shopping.includes('banana');         //moicavs tu ara array konkretul items//
+
+console.log(shopping);  
+
+console.log(shopping.length);    //ramdeni itemia arrayshi//
+
+console.log(shopping.includes('banana')); 
+
